d6c5c7a96ffc5fb9039a7bf54a4082fe5b98ec28
Lê Trung Kiên <kienlt.dv@gmail.com>
2025-06-04 22:50:24 +0700

    Auth Size
    Auth Size

--- src/main/java/com/clothingstore/clothing_store_api/controller/SizeController.java
+++ src/main/java/com/clothingstore/clothing_store_api/controller/SizeController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/sizes")
+@PreAuthorize("hasRole('ADMIN')")
 public class SizeController {
     @Autowired
     private SizeService sizeService;
