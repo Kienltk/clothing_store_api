e46a6a96df82b410af3d2c81bd038cbb4b3e38d2
Dinhchien2005 <154740465+Dinhchien2005@users.noreply.github.com>
2025-06-18 14:25:42 +0700

    Update ClothingStoreApiApplication.java

--- src/main/java/com/clothingstore/clothing_store_api/ClothingStoreApiApplication.java
+++ src/main/java/com/clothingstore/clothing_store_api/ClothingStoreApiApplication.java
@@ -10,7 +10,6 @@ public class ClothingStoreApiApplication {
 
     public static void main(String[] args) {
 //        System.out.println("Thu muc goc: " + new File(".").getAbsolutePath());
-        DotenvUtil.loadDotenv();
         SpringApplication.run(ClothingStoreApiApplication.class, args);
     }
 
