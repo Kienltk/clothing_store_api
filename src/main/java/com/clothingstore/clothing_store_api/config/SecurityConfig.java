71fbf58d7ee3a1f345a202c3ff6a0f060776a4e3
Dinhchien2005 <154740465+Dinhchien2005@users.noreply.github.com>
2025-06-18 20:13:02 +0700

    Update SecurityConfig.java

--- src/main/java/com/clothingstore/clothing_store_api/config/SecurityConfig.java
+++ src/main/java/com/clothingstore/clothing_store_api/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Đổi thành domain FE thật nếu cần
+        configuration.setAllowedOrigins(Arrays.asList("https://clothes-store-fe.onrender.com")); // Đổi thành domain FE thật nếu cần
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); //
