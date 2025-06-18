31d74e27158a44b69244639f53763390993500f7
nguyen dinh chien <Ndc7571@gmail.com>
2025-06-18 14:22:29 +0700

    test

--- src/main/java/com/clothingstore/clothing_store_api/ClothingStoreApiApplication.java
+++ src/main/java/com/clothingstore/clothing_store_api/ClothingStoreApiApplication.java
@@ -1,12 +1,8 @@
 package com.clothingstore.clothing_store_api;
 
 
-import com.clothingstore.clothing_store_api.config.LoadEnv;
-import com.clothingstore.clothing_store_api.util.DotenvUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
 
 
 @SpringBootApplication
--- src/main/java/com/clothingstore/clothing_store_api/config/LoadEnv.java
+++
@@ -1,9 +0,0 @@
-package com.clothingstore.clothing_store_api.config;
-
-import com.clothingstore.clothing_store_api.util.DotenvUtil;
-
-public class LoadEnv {
-    public static void main(String[] args) {
-
-    }
-}
