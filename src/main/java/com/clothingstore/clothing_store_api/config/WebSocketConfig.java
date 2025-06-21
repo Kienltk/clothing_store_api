588ff22f8afbe52a266bdf080f8e22bb9ed7b25d
Dinhchien2005 <154740465+Dinhchien2005@users.noreply.github.com>
2025-06-21 18:58:36 +0700

    Update WebSocketConfig.java

--- src/main/java/com/clothingstore/clothing_store_api/config/WebSocketConfig.java
+++ src/main/java/com/clothingstore/clothing_store_api/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("https://clothes-store-fe-5.onrender.com")  
                 .withSockJS();
     }
 }
