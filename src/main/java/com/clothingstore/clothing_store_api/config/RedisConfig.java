958a589e59391add0d1f0e3c570698b0b712dbae
Dinhchien2005 <154740465+Dinhchien2005@users.noreply.github.com>
2025-06-22 19:47:57 +0700

    Update RedisConfig.java

--- src/main/java/com/clothingstore/clothing_store_api/config/RedisConfig.java
+++ src/main/java/com/clothingstore/clothing_store_api/config/RedisConfig.java
@@ -2,11 +2,15 @@ package com.clothingstore.clothing_store_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -21,12 +25,11 @@ public class RedisConfig {
         redisConfig.setPort(6379);
         redisConfig.setPassword(RedisPassword.of("AWO6AAIjcDFiNWY2MWRjNTRmMzI0N2RkYjFjMmRjYzc5OTA3MGUzOXAxMA"));
 
-       
         LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
                 LettuceClientConfiguration.builder();
 
-        builder.useSsl(); 
-        builder.commandTimeout(Duration.ofSeconds(60)); 
+        builder.useSsl();
+        builder.commandTimeout(Duration.ofSeconds(60));
 
         LettuceClientConfiguration clientConfig = builder.build();
 
@@ -40,5 +43,17 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(LettuceConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
     }
 }
