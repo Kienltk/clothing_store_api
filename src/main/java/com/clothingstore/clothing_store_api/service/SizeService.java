653f1d12978f3beb3d0fd59023610f18b4131162
Dinhchien2005 <154740465+Dinhchien2005@users.noreply.github.com>
2025-06-22 20:27:56 +0700

    Update SizeService.java

--- src/main/java/com/clothingstore/clothing_store_api/service/SizeService.java
+++ src/main/java/com/clothingstore/clothing_store_api/service/SizeService.java
@@ -1,11 +1,12 @@
 package com.clothingstore.clothing_store_api.service;
 
-import com.clothingstore.clothing_store_api.dto.ColorDTO;
 import com.clothingstore.clothing_store_api.dto.SizeDTO;
-import com.clothingstore.clothing_store_api.entity.Color;
 import com.clothingstore.clothing_store_api.entity.Size;
 import com.clothingstore.clothing_store_api.repository.SizeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,12 +32,14 @@ public class SizeService {
         return size;
     }
 
+    @CacheEvict(value = "sizes", allEntries = true)
     public SizeDTO createSize(SizeDTO sizeDTO) {
         Size size = convertToEntity(sizeDTO);
         Size savedSize = sizeRepository.save(size);
         return convertToDTO(savedSize);
     }
 
+    @Cacheable(value = "sizes", key = "'all'")
     public List<SizeDTO> getAllSizes() {
         return sizeRepository.findAll()
                 .stream()
@@ -44,19 +47,22 @@ public class SizeService {
                 .collect(Collectors.toList());
     }
 
+    @Cacheable(value = "sizes", key = "#id")
     public Optional<SizeDTO> getSizeById(Long id) {
         return sizeRepository.findById(id)
                 .map(this::convertToDTO);
     }
 
+    @CachePut(value = "sizes", key = "#id")
     public SizeDTO updateSize(Long id, SizeDTO sizeDTO) {
         Size size = sizeRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Size not found with id: " + id));
         size.setSize(sizeDTO.getSize());
-        Size updatedSize= sizeRepository.save(size);
+        Size updatedSize = sizeRepository.save(size);
         return convertToDTO(updatedSize);
     }
 
+    @CacheEvict(value = "sizes", allEntries = true)
     public void deleteSize(Long id) {
         Size size = sizeRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Size not found with id: " + id));
